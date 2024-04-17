bafd131129ad7859d96083f74d79405ca4e7de95
YaroslavZuban <72079338+YaroslavZuban@users.noreply.github.com>
2024-04-17 16:38:46 +0700

    Добавил SQL запрос, чтобы получить информацию о поле type

--- src/main/java/com/example/serverparsing/repository/GenderRepository.java
+++ src/main/java/com/example/serverparsing/repository/GenderRepository.java
@@ -2,11 +2,13 @@ package com.example.serverparsing.repository;
 
 import com.example.serverparsing.entity.Gender;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface GenderRepository extends JpaRepository<Gender, Integer> {
+    @Query(value = "SELECT type FROM gender", nativeQuery = true)
     List<String> getGenderAll();
 }
