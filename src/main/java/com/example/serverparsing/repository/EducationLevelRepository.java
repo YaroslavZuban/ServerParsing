ee790d99ab18caa19a17c1cb89fbf746372a7b0e
YaroslavZuban <72079338+YaroslavZuban@users.noreply.github.com>
2024-04-25 16:48:52 +0700

    Добавил SQL запросы для аналитики данных - Добавил запрос который обрабатывает по университету и скилам - Добавил запрос который обрабатывает по университету, специальности и скилам - Добавил запрос который обрабатывает по университету, специальности, году выпуска и скилам

--- src/main/java/com/example/serverparsing/repository/EducationLevelRepository.java
+++ src/main/java/com/example/serverparsing/repository/EducationLevelRepository.java
@@ -7,10 +7,10 @@ import org.springframework.stereotype.Repository;
 
 import java.beans.JavaBean;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface EducationLevelRepository extends JpaRepository<EducationType, Integer> {
     @Query(value = "SELECT education_level FROM education_type ORDER BY education_level", nativeQuery = true)
     List<String> getEducationTypeAll();
-
 }
